f8ddb0f9aad0e7e11dc5de4127cd5ddf57f64cc3
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-01-29 16:52:42 +0900

    feat: registered quartzelv:quartz_elevator blockitem

--- src/main/java/atonkish/quartzelv/QuartzElevatorMod.java
+++ src/main/java/atonkish/quartzelv/QuartzElevatorMod.java
@@ -1,10 +1,24 @@
 package atonkish.quartzelv;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class QuartzElevatorMod implements ModInitializer {
+	public static final String MOD_ID = "quartzelv";
+
+	public static final Block QUARTZ_ELEVATOR_BLOCK = new Block(FabricBlockSettings.copy(Blocks.QUARTZ_BLOCK));
+
 	@Override
 	public void onInitialize() {
-		System.out.println("Hello Quartz Elevator Mod!");
+		Registry.register(Registry.BLOCK, new Identifier(MOD_ID, "quartz_elevator"), QUARTZ_ELEVATOR_BLOCK);
+		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "quartz_elevator"),
+				new BlockItem(QUARTZ_ELEVATOR_BLOCK, new Item.Settings().group(ItemGroup.DECORATIONS)));
 	}
 }
