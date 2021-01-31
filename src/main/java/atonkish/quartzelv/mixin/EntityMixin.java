25432a64bc144530bf932ad57fb2745388d83ece
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-01-31 13:45:41 +0900

    feat: added teleport mixins

---
+++ src/main/java/atonkish/quartzelv/mixin/EntityMixin.java
@@ -0,0 +1,55 @@
+package atonkish.quartzelv.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import atonkish.quartzelv.blocks.QuartzElevatorBlock;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+
+@Mixin(Entity.class)
+public abstract class EntityMixin {
+    @Shadow
+    public World world;
+    @Shadow
+    private Vec3d pos;
+
+    @Shadow
+    public abstract BlockPos getBlockPos();
+
+    @Shadow
+    public abstract void teleport(double dstX, double dstY, double dstZ);
+
+    @Inject(at = @At("HEAD"), method = "setSneaking", cancellable = true)
+    private void setSneaking(boolean sneaking, CallbackInfo info) {
+        if (sneaking) {
+            BlockPos blockPos = getBlockPos();
+            Block block = world.getBlockState(blockPos.down()).getBlock();
+
+            if (block instanceof QuartzElevatorBlock) {
+                blockPos = blockPos.down();
+                int topY = blockPos.getY();
+                for (; blockPos.getY() > topY - QuartzElevatorBlock.TELEPORTABLE_DISTANCE; blockPos = blockPos.down()) {
+                    if (blockPos.getY() <= 0) {
+                        break;
+                    } else if ((world.getBlockState(blockPos.down()).getBlock().equals(block))
+                            && QuartzElevatorBlock.isTeleportable(world, blockPos)) {
+                        world.playSound((PlayerEntity) null, blockPos, SoundEvents.ENTITY_ENDERMAN_TELEPORT,
+                                SoundCategory.BLOCKS, 1.0F, 1.0F);
+                        teleport(this.pos.x, blockPos.getY(), this.pos.z);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/atonkish/quartzelv/mixin/LivingEntityMixin.java
@@ -0,0 +1,45 @@
+package atonkish.quartzelv.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import atonkish.quartzelv.blocks.QuartzElevatorBlock;
+import net.minecraft.block.Block;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin extends Entity {
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(at = @At("HEAD"), method = "jump", cancellable = true)
+    private void jump(CallbackInfo info) {
+        BlockPos blockPos = getBlockPos();
+        Block block = world.getBlockState(blockPos.down()).getBlock();
+
+        if (block instanceof QuartzElevatorBlock) {
+            int bottomY = blockPos.down().getY();
+            for (; blockPos.getY() < bottomY + QuartzElevatorBlock.TELEPORTABLE_DISTANCE; blockPos = blockPos.up()) {
+                if (blockPos.getY() >= world.getHeight()) {
+                    break;
+                } else if ((world.getBlockState(blockPos.up()).getBlock().equals(block))
+                        && QuartzElevatorBlock.isTeleportable(world, blockPos.up(2))) {
+                    world.playSound((PlayerEntity) null, blockPos, SoundEvents.ENTITY_ENDERMAN_TELEPORT,
+                            SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    teleport(getX(), blockPos.up(2).getY(), getZ());
+                    break;
+                }
+            }
+        }
+    }
+}
