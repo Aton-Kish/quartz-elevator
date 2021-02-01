b36caa60faa556912402b0a767a5897f1b6ee09b
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-02-01 16:50:30 +0900

    fix: added client env annotation

--- src/main/java/atonkish/quartzelv/config/QuartzElevatorMenu.java
+++ src/main/java/atonkish/quartzelv/config/QuartzElevatorMenu.java
@@ -3,7 +3,10 @@ package atonkish.quartzelv.config;
 import io.github.prospector.modmenu.api.ConfigScreenFactory;
 import io.github.prospector.modmenu.api.ModMenuApi;
 import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
 public class QuartzElevatorMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
