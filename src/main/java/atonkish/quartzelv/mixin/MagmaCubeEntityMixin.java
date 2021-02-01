60e59d21b62a1cc6f3923de09c38f4d00ad644d4
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2021-02-01 15:18:38 +0900

    feat: applied to Slime and Magma Cube entities

---
+++ src/main/java/atonkish/quartzelv/mixin/MagmaCubeEntityMixin.java
@@ -0,0 +1,31 @@
+package atonkish.quartzelv.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import atonkish.quartzelv.QuartzElevatorMod;
+import atonkish.quartzelv.utils.MixinUtil;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.MagmaCubeEntity;
+import net.minecraft.world.World;
+
+@Mixin(MagmaCubeEntity.class)
+public abstract class MagmaCubeEntityMixin extends Entity {
+    public MagmaCubeEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(at = @At("HEAD"), method = "jump", cancellable = true)
+    private void jump(CallbackInfo info) {
+        if (!QuartzElevatorMod.CONFIG.isPlayerOnly) {
+            // `isPlayerOnly`: false -> Magma Cube entities can also teleport
+            MixinUtil.teleportUp(world, getBlockPos(), (Double y) -> {
+                teleport(getX(), y, getZ());
+                return (Void) null;
+            });
+        }
+    }
+}
---
+++ src/main/java/atonkish/quartzelv/mixin/SlimeEntityMixin.java
@@ -0,0 +1,31 @@
+package atonkish.quartzelv.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import atonkish.quartzelv.QuartzElevatorMod;
+import atonkish.quartzelv.utils.MixinUtil;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.world.World;
+
+@Mixin(SlimeEntity.class)
+public abstract class SlimeEntityMixin extends Entity {
+    public SlimeEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(at = @At("HEAD"), method = "jump", cancellable = true)
+    private void jump(CallbackInfo info) {
+        if (!QuartzElevatorMod.CONFIG.isPlayerOnly) {
+            // `isPlayerOnly`: false -> Slime entities can also teleport
+            MixinUtil.teleportUp(world, getBlockPos(), (Double y) -> {
+                teleport(getX(), y, getZ());
+                return (Void) null;
+            });
+        }
+    }
+}
