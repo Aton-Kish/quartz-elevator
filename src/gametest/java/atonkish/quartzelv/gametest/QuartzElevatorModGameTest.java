b0fabe492c6ba9363d2a4550a879f28b419332c9
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-05-12 22:25:21 +0900

    added gametest batch ids

---
+++ src/gametest/java/atonkish/quartzelv/gametest/QuartzElevatorModGameTest.java
@@ -0,0 +1,7 @@
+package atonkish.quartzelv.gametest;
+
+public class QuartzElevatorModGameTest {
+    public static final String BATCH_ID_LOOT_TABLE = "lootTableBatch";
+    public static final String BATCH_ID_TELEPORT = "teleportBatch";
+    public static final String BATCH_ID_TELEPORT_WITH_MIX_TYPES = "teleportWithMixTypesBatch";
+}
