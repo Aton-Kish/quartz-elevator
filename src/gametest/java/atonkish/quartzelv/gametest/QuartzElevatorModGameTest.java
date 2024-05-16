e7a36b1d91294bd87174959bfc6c63c5e7225664
Aton-Kish <38515249+Aton-Kish@users.noreply.github.com>
2024-05-16 23:54:27 +0900

    added recipe tests

--- src/gametest/java/atonkish/quartzelv/gametest/QuartzElevatorModGameTest.java
+++ src/gametest/java/atonkish/quartzelv/gametest/QuartzElevatorModGameTest.java
@@ -2,6 +2,7 @@ package atonkish.quartzelv.gametest;
 
 public class QuartzElevatorModGameTest {
     public static final String BATCH_ID_LOOT_TABLE = "lootTableBatch";
+    public static final String BATCH_ID_RECIPE = "recipeBatch";
     public static final String BATCH_ID_TELEPORT = "teleportBatch";
     public static final String BATCH_ID_TELEPORT_WITH_MIX_TYPES = "teleportWithMixTypesBatch";
 }
